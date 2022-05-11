8c0f1cef8e156841f10e5bbb11c63e7a23923538
foxtrot-99 <55873929+foxtrot-99@users.noreply.github.com>
2022-05-11 19:23:59 +0900

    완쪽 도구 막대

--- static/js/layout.js
+++ static/js/layout.js
@@ -13,4 +13,30 @@ $(function() {
 	$('nav a.dropdown-toggle').click(function() {
 		$(this).next().slideToggle('fast');
 	});
+	
+	$('table.body td.task-td dl.tasks > dt').click(function() {
+		$(this).next().slideToggle('fast');
+	});
+	
+	var recentCard = $('table.body td.task-td dl.tasks.recent-changes > dd');
+		
+	$.ajax({
+		url: '/sidebar.json',
+		dataType: 'json',
+		success: function(data) {
+			var ret = '';
+			var i;
+			for(i=0; i<9; i++) {
+				var item = data[i];
+				if(!item) break;
+				var date = new Date(item.date * 1000);
+				var time = date.getHours() + ':' + date.getMinutes();
+				ret += '<li>[' + time + '] <a href="/w/' + encodeURIComponent(item.document) + '">' + item.document + '</a></li>';
+			}
+			
+			recentCard.html('<ul class=wiki-list>' + ret + '</ul>');
+		}, error: function() {
+			recentCard.html('<li>갱신 실패!</li>');
+		}
+	});
 });
